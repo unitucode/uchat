d278532b63202cd8fc1558b33566aa999a104e65
vstruk01 <stoliar444@gmail.com>
2020-05-12 19:56:55 +0300

    fix create_table_user and update_permission

--- src/utils/Database/update_permission_of_user.c
+++ src/utils/Database/update_permission_of_user.c
@@ -1,15 +1,15 @@
 #include <utils.h>
 
-void update_permission_of_user(unsigned int permission, char *login, sqlite3 *database) {
+void mx_update_permission_of_user(unsigned int permission, char *login, sqlite3 *database) {
     sqlite3_stmt *stmt;
     int returnvalue;
 
-    sqlite3_prepare_v3(database, "UPDATE USER SET PERMISSION = ?1 WHERE token = ?2", -1, 0, &stmt, NULL);
+    sqlite3_prepare_v3(database, "UPDATE USER set PERMISSION = ?1 WHERE login = ?2", -1, 0, &stmt, NULL);
     sqlite3_bind_int(stmt, 1, permission);
     sqlite3_bind_text(stmt, 2, login, -1, SQLITE_STATIC);
     
-    if ((returnvalue = sqlite3_step(stmt)) != SQLITE_ROW) {
-        printf("error get user !");
+    if ((returnvalue = sqlite3_step(stmt)) != SQLITE_DONE) {
+        mx_logger(MX_LOG_FILE, LOGWAR, "error update permission");
     }
     sqlite3_finalize(stmt);
 }
