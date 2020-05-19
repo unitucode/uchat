e6c4d5c85dc7cb79b445f00f4b7017606dc6be46
Dumagenon <simonov.pasha9822@gmail.com>
2020-05-19 13:15:25 +0300

    comit

--- src/server/client_handler/sign_up.c
+++ src/server/client_handler/sign_up.c
@@ -3,6 +3,7 @@
 
 bool mx_sign_up(t_dtp *login, t_client *client) {
     cJSON *login_struct = NULL;
+    
     if (login->type != MX_SIGN_UP)
         return false;
     login_struct = cJSON_GetObjectItemCaseSensitive(login->json, "login");
