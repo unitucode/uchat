91f1020b67b5c812d57fcbfbf2af5a69462c8346
ymakohon <yuriimakohon@gmail.com>
2020-05-19 14:13:00 +0300

    take out mx_valid_authorization() to separate file; prototype fixed

--- inc/utils_inc/protocol/protocol.h
+++ inc/utils_inc/protocol/protocol.h
@@ -54,4 +54,4 @@ t_dtp *mx_request_creation(char *req_body);
 void mx_free_request_struct(t_dtp **request);
 
 //Authorization
-bool mx_valid_authorization_data(t_dtp *data, char **login_str, char **pass_str);
+bool mx_valid_authorization_data(t_dtp *data, char **login, char **pass);
