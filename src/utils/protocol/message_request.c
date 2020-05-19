12d1ad3e7e899dca7c68888110d9d648f368c90e
AIexMoran <homin.sashawork@gmail.com>
2020-05-19 15:54:40 +0300

    fixed token log_in

--- src/utils/protocol/message_request.c
+++ src/utils/protocol/message_request.c
@@ -37,19 +37,3 @@ t_dtp *mx_error_msg_request(int error_code, char *msg) {
     cJSON_Delete(json_result);
     return dtp;
 }
-
-t_dtp *mx_token_request(char *token) {
-    t_dtp *dtp = NULL;
-    cJSON *json_result = cJSON_CreateObject();
-    char *json_str = NULL;
-
-    if (!cJSON_AddNumberToObject(json_result, "type", MX_TOKEN))
-        return NULL;
-    if (!cJSON_AddStringToObject(json_result, "token", MX_J_STR(token)))
-        return NULL;
-    json_str = cJSON_Print(json_result);
-    dtp = mx_request_creation(json_str);
-    mx_free((void**)&json_str);
-    cJSON_Delete(json_result);
-    return dtp;
-}
