0804ff64ac2af0b4321bab387819a59c04bfdb61
Dumagenon <simonov.pasha9822@gmail.com>
2020-05-23 12:10:33 +0300

    Refactoring request functions

--- src/utils/protocol/authorization_request.c
+++ src/utils/protocol/authorization_request.c
@@ -1,9 +1,7 @@
 #include "protocol.h"
 
 t_dtp *mx_log_in_request(char *login, char *pass) {
-    t_dtp *dtp = NULL;
     cJSON *json_result = cJSON_CreateObject();
-    char *json_str = NULL;
 
     if (!cJSON_AddNumberToObject(json_result, "type", MX_LOG_IN))
         return NULL;
@@ -11,17 +9,11 @@ t_dtp *mx_log_in_request(char *login, char *pass) {
         return NULL;
     if (!cJSON_AddStringToObject(json_result, "pass", MX_J_STR(pass)))
         return NULL;
-    json_str = cJSON_Print(json_result);
-    dtp = mx_request_creation(json_str);
-    mx_free((void**)&json_str);
-    cJSON_Delete(json_result);
-    return dtp;
+    return mx_get_transport_data(json_result);
 }
 
 t_dtp *mx_sign_up_request(char *login, char *pass) {
-    t_dtp *dtp = NULL;
     cJSON *json_result = cJSON_CreateObject();
-    char *json_str = NULL;
 
     if (!cJSON_AddNumberToObject(json_result, "type", MX_SIGN_UP))
         return NULL;
@@ -29,25 +21,15 @@ t_dtp *mx_sign_up_request(char *login, char *pass) {
         return NULL;
     if (!cJSON_AddStringToObject(json_result, "pass", MX_J_STR(pass)))
         return NULL;
-    json_str = cJSON_Print(json_result);
-    dtp = mx_request_creation(json_str);
-    mx_free((void**)&json_str);
-    cJSON_Delete(json_result);
-    return dtp;
+    return mx_get_transport_data(json_result);
 }
 
 t_dtp *mx_token_request(char *token) {
-    t_dtp *dtp = NULL;
     cJSON *json_result = cJSON_CreateObject();
-    char *json_str = NULL;
 
     if (!cJSON_AddNumberToObject(json_result, "type", MX_TOKEN))
         return NULL;
     if (!cJSON_AddStringToObject(json_result, "token", MX_J_STR(token)))
         return NULL;
-    json_str = cJSON_Print(json_result);
-    dtp = mx_request_creation(json_str);
-    mx_free((void**)&json_str);
-    cJSON_Delete(json_result);
-    return dtp;
+    return mx_get_transport_data(json_result);
 }
