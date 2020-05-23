0bc4a6568e27b354082b8b71e3e2ecaf539dc4a0
vstruk01 <stoliar444@gmail.com>
2020-05-23 13:21:48 +0300

    test

--- src/utils/database/create_json_object.c
+++ src/utils/database/create_json_object.c
@@ -16,8 +16,7 @@ static cJSON *get_object_message(sqlite3_stmt *stmt) {
     return object_message;
 }
 
-static cJSON *get_message_arr(char *name_room, unsigned int id_room,
-                              sqlite3 *database) {
+static cJSON *get_message_arr(char *name_room, sqlite3 *database) {
     cJSON *arr_object = cJSON_CreateArray();
     sqlite3_str *str = sqlite3_str_new(database);
     sqlite3_stmt *stmt;
@@ -26,7 +25,6 @@ static cJSON *get_message_arr(char *name_room, unsigned int id_room,
     
     sqlite3_str_appendall(str, "SELECT * FROM ");
     sqlite3_str_appendall(str, name_room);
-    sqlite3_str_appendall(str, " WHERE ID_ROOM = ?1");
     sql = sqlite3_str_finish(str);
     rv = sqlite3_prepare_v3(database, sql, -1, 0, &stmt, NULL);
     sqlite3_bind_int(stmt, 1, id_room);
@@ -39,8 +37,7 @@ static cJSON *get_message_arr(char *name_room, unsigned int id_room,
     return arr_object;
 }
 
-static cJSON *get_data_room(unsigned int id_room, sqlite3 *database,
-                            sqlite3_stmt *stmt) {
+static cJSON *get_data_room(sqlite3 *database, sqlite3_stmt *stmt) {
     cJSON *object_tmp = cJSON_CreateObject();
     char *name_room = NULL;
 
@@ -52,7 +49,7 @@ static cJSON *get_data_room(unsigned int id_room, sqlite3 *database,
     cJSON_AddItemToObject(object_tmp, "customer_login",
         cJSON_CreateString((char*)sqlite3_column_text(stmt, 2)));
     cJSON_AddItemToObject(object_tmp, "message",
-        get_message_arr(name_room, id_room, database));
+        get_message_arr(name_room, database));
     return object_tmp;
 }
 
@@ -102,4 +99,11 @@ cJSON *mx_create_json_object(sqlite3 *database, char *user_login) {
     return get_json(database, save);
 }
 
-// cJSON *
+cJSON *mx_create_json_message(sqlite3 *database, 
+                              char *name_room, long long date) {
+    cJSON *room = cJSON_CreateObject();
+    cJSON *message = cJSON_CreateArray();
+
+    cJSON_AddItemToObject(room, "name_room", cJSON_CreateString(name_room));
+
+}
