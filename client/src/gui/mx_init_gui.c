d276030ae662ea6937c999cfb475f92d39855fde
ymakohon <yuriimakohon@gmail.com>
2020-05-27 23:38:46 +0300

    added simple authorization wnd

--- client/src/gui/mx_init_gui.c
+++ client/src/gui/mx_init_gui.c
@@ -9,6 +9,18 @@ void mx_req_addroom(GtkButton *btn, t_chat *chat) {
     (void)btn;
 }
 
+void mx_req_signup(GtkButton *btn, t_chat *chat) {
+    (void)btn;
+    (void)chat;
+    puts("signup");
+}
+
+void mx_req_login(GtkButton *btn, t_chat *chat) {
+    (void)btn;
+    (void)chat;
+    puts("login");
+}
+
 static void connect_addroom(t_chat *chat) {
     GtkButton *btn = GTK_BUTTON(gtk_builder_get_object(chat->builder,
                                                        "btn_addroom_apply"));
@@ -16,6 +28,17 @@ static void connect_addroom(t_chat *chat) {
     g_signal_connect(btn, "clicked", G_CALLBACK(mx_req_addroom), chat);
 }
 
+static void connect_authorization(t_chat *chat) {
+    GtkButton *btn_signup = GTK_BUTTON(gtk_builder_get_object(chat->builder,
+                                                       "btn_signup"));
+    GtkButton *btn_login = GTK_BUTTON(gtk_builder_get_object(chat->builder,
+                                                       "btn_login"));
+
+    g_signal_connect(btn_signup, "clicked", G_CALLBACK(mx_req_signup), chat);
+    g_signal_connect(btn_login, "clicked", G_CALLBACK(mx_req_login), chat);
+}
+
 void mx_init_gui(t_chat *chat) {
     connect_addroom(chat);
+    connect_authorization(chat);
 }
