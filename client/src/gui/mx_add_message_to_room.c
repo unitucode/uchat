dc1df7c694495aac44f04191f03d104e00712fd7
Dumagenon <simonov.pasha9822@gmail.com>
2020-06-09 14:48:04 +0300

    double buffer_message clear fixed

--- client/src/gui/mx_add_message_to_room.c
+++ client/src/gui/mx_add_message_to_room.c
@@ -2,13 +2,13 @@
 
 static void add_message_row(t_gmsg *msg, GtkBuilder *builder) {
     GtkWidget *row = mx_create_message_row(msg);
-    GtkWidget *box = mx_get_groom_by_name(msg->room_name, builder)->box_messages;
+    GtkWidget *box = NULL;
 
+    box = mx_get_groom_by_name(msg->room_name, builder)->box_messages;
     gtk_list_box_insert(GTK_LIST_BOX(box), row, -1);
     gtk_widget_show_all(GTK_WIDGET(row));
 }
 
 void mx_add_message_to_room(t_gmsg *msg, GtkBuilder *builder) {
-    mx_clear_buffer_text("buffer_message", builder);
     add_message_row(msg, builder);
 }
