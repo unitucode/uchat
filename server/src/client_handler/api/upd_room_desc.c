44df1312386c76f657d02dbafced4326e52467ef
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-12 14:44:35 +0300

    working with room desc

--- server/src/client_handler/api/upd_room_desc.c
+++ server/src/client_handler/api/upd_room_desc.c
@@ -3,7 +3,7 @@
 t_dtp *mx_upd_room_desc_request(int room_id, char *desc) {
     cJSON *json_result = cJSON_CreateObject();
 
-    if (!cJSON_AddNumberToObject(json_result, "type", RQ_LOG_OUT))
+    if (!cJSON_AddNumberToObject(json_result, "type", RQ_UPD_ROOM_DESC))
         return NULL;
     if (!cJSON_AddNumberToObject(json_result, "room_id", room_id))
         return NULL;
