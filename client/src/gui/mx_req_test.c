e9db46b0da000882533355713ad8a9f19bca8aeb
Yurii Makohon <ymakohon@e1r4p16.unit.ua>
2020-06-12 12:30:45 +0300

    added mx_req_test func

---
+++ client/src/gui/mx_req_test.c
@@ -0,0 +1,7 @@
+#include "client.h"
+
+void mx_req_test(GtkButton *btn, GtkBuilder *builder) {
+    puts("TEST REQUEST");
+    (void)btn;
+    (void)builder; // Remove this string when use builder
+}
