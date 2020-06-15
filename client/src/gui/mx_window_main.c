96fa45dcf15e9fe7b435b1799bddf0fd4ae4eb71
Pavlo Symonov <psymonov@e1r6p16.unit.ua>
2020-06-15 15:36:10 +0300

    Css added

--- client/src/gui/mx_window_main.c
+++ client/src/gui/mx_window_main.c
@@ -12,10 +12,10 @@ GtkBuilder *mx_init_window(int argc, char **argv) {
 }
 
 int mx_start_gui(t_chat *chat) {
-    GdkPixbuf *svg = gdk_pixbuf_new_from_file("../src/gui/resources/image.png", true, NULL);
-    GtkWidget *img = gtk_image_new_from_pixbuf(svg);
-    GObject *button = gtk_builder_get_object(chat->builder, "btn_show_roomlist");
-    gtk_button_set_image(GTK_BUTTON(button), img);
+    // GdkPixbuf *svg = gdk_pixbuf_new_from_file("../src/gui/resources/web-browser.tiff", NULL);
+    // GtkWidget *img = gtk_image_new_from_pixbuf(svg);
+    // GObject *button = gtk_builder_get_object(chat->builder, "btn_show_roomlist");
+    // gtk_button_set_image(GTK_BUTTON(button), img);
     
     gtk_main();
 
