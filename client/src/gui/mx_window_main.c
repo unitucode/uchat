5067dd9d2d6e90e80b8a694822181202c33bcfa4
Pavlo Symonov <psymonov@e1r10p10.unit.ua>
2020-06-19 16:12:27 +0300

    Yes, images work

--- client/src/gui/mx_window_main.c
+++ client/src/gui/mx_window_main.c
@@ -12,13 +12,30 @@ GtkBuilder *mx_init_window(int argc, char **argv) {
 }
 
 int mx_start_gui(t_chat *chat) {
-    // GdkPixbuf *svg = gdk_pixbuf_new_from_file("../src/gui/resources/web-browser.tiff", NULL);
-    // GtkWidget *img = gtk_image_new_from_pixbuf(svg);
-    // GObject *button = gtk_builder_get_object(chat->builder, "btn_show_roomlist");
-    // gtk_button_set_image(GTK_BUTTON(button), img);
+
+    // GdkPixbuf *hilvl = gdk_pixbuf_new_from_file("../src/gui/resources/attach(1).png", NULL);
     
-    GtkIconTheme *icon_theme = gtk_icon_theme_get_default();;
-    gtk_icon_theme_add_resource_path(icon_theme, "../src/gui/resources");
+    // GdkPixbuf *new = gdk_pixbuf_scale_simple(hilvl, 50, 50, GDK_INTERP_HYPER);
+    
+    // GtkWidget *img = gtk_image_new_from_pixbuf(new);
+    // GObject *button = gtk_builder_get_object(chat->builder, "btn_stickers");
+    // gtk_button_set_image(GTK_BUTTON(button), img);
+
+    // GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file("../src/gui/resources/attach(1).png", NULL);
+    // int width, height;
+
+    // gdk_pixbuf_get_file_info("../src/gui/resources/attach(1).png", &width, &height);
+    // gtk_icon_theme_add_builtin_icon("link_icon", width, pixbuf);
+    // g_object_unref (G_OBJECT (pixbuf)); 
+
+    // GObject *button = gtk_builder_get_object(chat->builder, "btn_stickers");
+    // GtkWidget *img = gtk_image_new_from_icon_name("link_icon", 18);
+    // gtk_button_set_image (GTK_BUTTON(button), img);
+    const gchar *path[] = {"../src/gui/resources"};
+    gtk_icon_theme_set_search_path(gtk_icon_theme_get_default(), path, 1);
+    GObject *button = gtk_builder_get_object(chat->builder, "btn_stickers");
+    GtkWidget *img = gtk_image_new_from_icon_name("attach(1)", GTK_ICON_SIZE_LARGE_TOOLBAR);
+    gtk_button_set_image (GTK_BUTTON(button), img);
 
     gtk_main();
 
