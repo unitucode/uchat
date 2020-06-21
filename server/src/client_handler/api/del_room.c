458dec6db6d42054a3147ce63c928582628740e3
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-21 16:23:14 +0300

    working with room

--- server/src/client_handler/api/del_room.c
+++ server/src/client_handler/api/del_room.c
@@ -15,20 +15,15 @@ static t_dtp *get_resend_room(int room_id) {
 bool mx_del_room_handler(t_dtp *data, t_client *client) { //TODO leaks
     cJSON *room_id = cJSON_GetObjectItemCaseSensitive(data->json,
                                                    "room_id");
-    t_db_room *room = NULL;
     t_dtp *resend = NULL;
 
     if (!room_id || !cJSON_IsNumber(room_id))
         return false;
-    room = mx_get_room_by_id(client->info->database, room_id->valueint);
-    if (!room || room->customer_id != client->user->user_id) {
-        mx_free_room(&room);
+    if (mx_get_type_member(client->info->database, client->user->user_id, room_id->valueint) != DB_CUSTOMER)
         return false;
-    }
     mx_delete_room_by_id(client->info->database, room_id->valueint);
     resend = get_resend_room(room_id->valueint);
     mx_send_to_all(resend, client, room_id->valueint);
     mx_free_request(&resend);
-    mx_free_room(&room);
     return true;
 }
--- server/src/client_handler/api/upd_room_desc.c
+++ server/src/client_handler/api/upd_room_desc.c
@@ -22,12 +22,9 @@ bool mx_upd_room_desc_handler(t_dtp *room, t_client *client) {
         return false;
     if (!desc || !cJSON_IsString(desc))
         return false;
-    // room_db = mx_get_room_by_id(client->info->database, room_id->valueint);
-    // if (!room_db || strcmp(room_db->customer, client->user->login)) {
-    //     mx_free_room(&room_db);
-    //     return false;
-    // }
-    // mx_update_description_room_by_id(client->chat->database, room_id->valueint, desc->valuestring);
+    if (mx_get_type_member(client->info->database, client->user->user_id, room_id->valueint) != DB_CUSTOMER)
+        return false;
+    mx_edit_desc_room_by_id(client->info->database, room_id->valueint, desc->valuestring);
     resend = mx_upd_room_desc_request(room_id->valueint, desc->valuestring);
     if (resend)
         mx_send_to_all(resend, client, room_id->valueint);
