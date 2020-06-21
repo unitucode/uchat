6b440817f70622e9056b08c544f26682791ff312
vstruk01 <stoliar444@gmail.com>
2020-06-21 16:05:17 +0300

    merge server

--- server/src/database/message.c
+++ server/src/database/message.c
@@ -1,12 +1,21 @@
 #include "server.h"
 
-void mx_blablabla() {
-    
+gboolean mx_is_owner_msg(sqlite3 *db, guint64 user_id, guint64 msg_id) {
+    sqlite3_stmt *stmt;
+    gint32 rv = 0;
+    gboolean is_owner = false;
+
+    rv = sqlite3_prepare_v2(db, "select permission from members where "
+                                "room_id = ?1 and user_id = ?2 ",
+                            -1, &stmt, NULL);
+    mx_error_sqlite(rv, "step", "is owner");
+    sqlite3_bind_int64(stmt, 1, room_id);
+    sqlite3_bind_int64(stmt, 2, user_id);
+    if ((rv = sqlite3_step(stmt)) == SQLITE_ROW)
+        is_owner = true;
+    mx_error_sqlite(rv, "step", "is owner");
+    sqlite3_finalize(stmt);
+    return is_owner;
 }
-
-// gboolean mx_is_owner_msg(sqlite3 *db, guint64 user_id, guint64 msg_id) {
-
-//     return true;
-// }
 
 
