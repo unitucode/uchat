21e36a59d126391c182e86732208cf90f639976e
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-23 15:44:07 +0300

    working with formatting

--- client/src/gui/gui-utils/formatting_text.c
+++ client/src/gui/gui-utils/formatting_text.c
@@ -9,18 +9,48 @@ void mx_text_buffer_set_tags(GtkTextBuffer *buffer) {
 }
 
 static void set_tags(GtkTextBuffer *buffer, gchar *ft_chars) {
-    GtkTextIter start;
-    GtkTextIter end;
-    gchar *text = NULL;
+    // GtkTextIter start;
+    // GtkTextIter end;
+    // gchar *text = NULL;
+    // gint start_offset = 0;
 
-    gtk_text_buffer_get_iter_at_offset(buffer, &start, 0);
-    gtk_text_buffer_get_iter_at_offset(buffer, &end, gtk_text_buffer_get_char_count(buffer));
-    text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
-    gtk_text_buffer_get_iter_at_offset(buffer, &start, g_strrstr(text, ft_chars) - text + strlen(ft_chars));
-    g_print("start = %d\n", gtk_text_iter_get_offset(&start));
-    gtk_text_buffer_get_iter_at_offset(buffer, &end, g_strrstr(text + gtk_text_iter_get_offset(&start), ft_chars) - text);
-    g_print("end = %d\n", gtk_text_iter_get_offset(&end));
-    gtk_text_buffer_apply_tag_by_name(buffer, ft_chars, &start, &end);
+    // gtk_text_buffer_get_iter_at_offset(buffer, &start, 0);
+    // gtk_text_buffer_get_iter_at_offset(buffer, &end, gtk_text_buffer_get_char_count(buffer));
+    // text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
+    // while ((unsigned long)(text - save) <= strlen(save) && g_strstr_len(text, strlen(text), ft_chars)) {
+    //     gtk_text_buffer_get_iter_at_offset(buffer, &start, g_strstr_len(text, strlen(text), ft_chars) - text + strlen(ft_chars));
+    //     start_offset = gtk_text_iter_get_offset(&start);
+    //     gtk_text_buffer_get_iter_at_offset(buffer, &end, g_strstr_len(text + start_offset, strlen(text + start_offset), ft_chars) - text);
+    //     gtk_text_buffer_apply_tag_by_name(buffer, ft_chars, &start, &end);
+    //     gtk_text_iter_set_offset(&start, 0);
+    //     gtk_text_iter_set_offset(&end, gtk_text_buffer_get_char_count(buffer));
+    //     text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
+    // }
+    // GtkTextIter start;
+    // GtkTextIter end;
+    // gchar *text = NULL;
+    // gchar *sub = NULL;
+
+    // gtk_text_buffer_get_iter_at_offset(buffer, &start, 0);
+    // gtk_text_buffer_get_iter_at_offset(buffer, &end, gtk_text_buffer_get_char_count(buffer));
+    // text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
+    // while ((sub = g_strstr_len(text, strlen(text), ft_chars))) {
+    //     gtk_text_iter_set_offset(&start, sub - text);
+    //     gtk_text_iter_set_offset(&end, sub - text + strlen(ft_chars));
+    //     gtk_text_buffer_delete(buffer, &start, &end);
+    //     text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
+    //     sub = g_strstr_len(text, strlen(text), ft_chars);
+    //     gtk_text_iter_set_offset(&end, sub - text);
+    //     gtk_text_buffer_apply_tag_by_name(buffer, ft_chars, &start, &end);
+    //     gtk_text_iter_set_offset(&start, sub - text);
+    //     gtk_text_iter_set_offset(&end, sub - text + strlen(ft_chars));
+    //     gtk_text_buffer_delete(buffer, &start, &end);
+    //     gtk_text_iter_set_offset(&start, 0);
+    //     gtk_text_iter_set_offset(&end, gtk_text_buffer_get_char_count(buffer));
+    //     text = gtk_text_buffer_get_text(buffer, &start, &end, TRUE);
+    // }
+    (void)buffer;
+    (void)ft_chars;
 }
 
 void mx_format_text(GtkTextBuffer *buffer) {
@@ -30,7 +60,11 @@ void mx_format_text(GtkTextBuffer *buffer) {
     gtk_text_buffer_get_iter_at_offset(buffer, &start, 0);
     gtk_text_buffer_get_iter_at_offset(buffer, &end, gtk_text_buffer_get_char_count(buffer));
     gtk_text_buffer_remove_all_tags(buffer, &start, &end);
+    set_tags(buffer, MX_FT_IMPORTANT);
+    set_tags(buffer, MX_FT_ITALIC);
     set_tags(buffer, MX_FT_UNDER);
+    set_tags(buffer, MX_FT_SCRATCH);
+    set_tags(buffer, MX_FT_BOLD);
 }
 
 
