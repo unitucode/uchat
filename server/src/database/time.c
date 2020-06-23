2e724861a233a4b035abcbb2ab742280872ee3e8
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-23 20:45:24 +0300

    Revert "Merge remote-tracking branch 'origin/vlad' into server"
    This reverts commit 72580864bc02cabb02c77f75dc2a6162849ea010, reversing
    changes made to 6b85591eec4e7b0cd4ea3577850d8889f6464d1b.

--- server/src/database/time.c
+++ server/src/database/time.c
@@ -2,15 +2,11 @@
 
 guint64 mx_get_time(gint8 type) {
     guint64 date = g_get_real_time();
-    guint64 time;
 
-    date += (3 * 60 * 60 * 100 * 100);
     if (type == DB_SECOND)
-        time = date / 1000000;
-    else if (type == DB_MILISECOND)
-        time = date / 1000;
-    else
-        time = date;
-    return time;
+        return date / 10000000;
+    if (type == DB_MILISECOND)
+        return date / 1000;
+    return date;
 }
 
