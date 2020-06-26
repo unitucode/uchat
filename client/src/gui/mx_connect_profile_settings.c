d2bed7349f9b888ad02aae2ab3ff2f59e941e500
Dumagenon <simonov.pasha9822@gmail.com>
2020-06-26 15:38:23 +0300

    added show member info

--- client/src/gui/mx_connect_profile_settings.c
+++ client/src/gui/mx_connect_profile_settings.c
@@ -1,5 +1,13 @@
 #include "client.h"
 
+static void req_get_member_info(GObject *popup, t_chat *chat) {
+    t_dtp *dtp = mx_member_info_request((int)g_object_get_data(popup,
+                                                               "member_id"));
+
+    mx_send(chat->out, dtp);
+    mx_free_request(&dtp);
+}
+
 static void req_logout(GtkButton *btn, t_chat *chat) {
     t_dtp *logout = mx_log_out_request(chat->auth_token);
 
@@ -11,6 +19,8 @@ static void req_logout(GtkButton *btn, t_chat *chat) {
 void mx_connect_profile_settings(t_chat *chat) {
     GtkButton *btn = GTK_BUTTON(gtk_builder_get_object(chat->builder,
                                                        "btn_logout"));
+    GObject *popup = gtk_builder_get_object(chat->builder, "popup_user_info");
 
     g_signal_connect(btn, "clicked", G_CALLBACK(req_logout), chat);
+    g_signal_connect(popup, "show", G_CALLBACK(req_get_member_info), chat);
 }
--- client/src/gui/mx_set_room_members.c
+++ client/src/gui/mx_set_room_members.c
@@ -1,5 +1,13 @@
 #include "client.h"
 
+static void show_user_info(GObject *btn, GtkBuilder *builder) {
+    GObject *popup = gtk_builder_get_object(builder, "popup_user_info");
+
+    g_object_set_data(popup, "member_id",
+                      g_object_get_data(btn, "member_id"));
+    mx_widget_set_visibility(GTK_WIDGET(popup), TRUE);
+}
+
 static void show_ban_member(GObject *btn, GtkBuilder *builder) {
     GObject *dialog = gtk_builder_get_object(builder, "dialog_ban_member");
     GObject *btn_ban = gtk_builder_get_object(builder, "btn_ban");
@@ -35,11 +43,17 @@ static void add_to_info_members(gint *key,
                                 gchar *value, GtkBuilder *builder) {
     GObject *listbox = gtk_builder_get_object(builder,
                                               "listbox_info_members");
+    GtkWidget *box_member = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     GtkWidget *name = gtk_label_new(value);
+    GtkWidget *btn = gtk_button_new_with_label("Info");
 
-    gtk_widget_set_size_request(name, -1, 40);
-    gtk_list_box_insert(GTK_LIST_BOX(listbox), name, -1);
+    gtk_widget_set_size_request(box_member, -1, 40);
+    gtk_box_pack_start(GTK_BOX(box_member), name, FALSE, FALSE, 0);
+    gtk_box_pack_end(GTK_BOX(box_member), btn, FALSE, FALSE, 0);
+    gtk_list_box_insert(GTK_LIST_BOX(listbox), box_member, -1);
     gtk_widget_show_all(GTK_WIDGET(listbox));
+    g_object_set_data(G_OBJECT(btn), "member_id", key);
+    g_signal_connect(btn, "clicked", G_CALLBACK(show_user_info), builder);
     (void)key;
 }
 
