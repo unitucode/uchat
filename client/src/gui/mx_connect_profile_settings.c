82cc7a3d95cd9f8e3cbf7eb2514b3bdca6ea7b82
Yurii Makohon <yuriimakohon@gmail.com>
2020-06-27 14:46:05 +0300

    user desc added

--- client/src/gui/mx_connect_profile_settings.c
+++ client/src/gui/mx_connect_profile_settings.c
@@ -35,14 +35,27 @@ static void req_logout(GtkButton *btn, t_chat *chat) {
     (void)btn;
 }
 
+static void set_profile_info(GtkButton *btn, t_chat *chat) {
+    GObject *label_login = gtk_builder_get_object(chat->builder,
+                                                  "label_user_login");
+    GObject *buff_desc = gtk_builder_get_object(chat->builder,
+                                                "buffer_user_desc");
+
+    gtk_label_set_text(GTK_LABEL(label_login), chat->login);
+    gtk_text_buffer_set_text(GTK_TEXT_BUFFER(buff_desc), chat->desc, -1);
+    (void)btn;
+}
+
 void mx_connect_profile_settings(t_chat *chat) {
     GtkButton *btn = GTK_BUTTON(gtk_builder_get_object(chat->builder,
                                                        "btn_logout"));
     GtkButton *edit = GTK_BUTTON(gtk_builder_get_object(chat->builder,
                                                         "btn_edit_profile"));
     GObject *popup = gtk_builder_get_object(chat->builder, "popup_user_info");
+    GObject *sett = gtk_builder_get_object(chat->builder, "btn_profile_sett");
 
     g_signal_connect(btn, "clicked", G_CALLBACK(req_logout), chat);
     g_signal_connect(edit, "clicked", G_CALLBACK(req_edit_desc), chat);
     g_signal_connect(popup, "show", G_CALLBACK(req_get_member_info), chat);
+    g_signal_connect(sett, "clicked", G_CALLBACK(set_profile_info), chat);
 }
