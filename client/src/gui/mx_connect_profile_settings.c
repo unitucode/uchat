3bd13e646dfed5c7aa9049c8d2b52bf8b13ef080
Yurii Makohon <yuriimakohon@gmail.com>
2020-06-27 14:58:18 +0300

    fixed profile desc

--- client/src/gui/mx_connect_profile_settings.c
+++ client/src/gui/mx_connect_profile_settings.c
@@ -37,12 +37,13 @@ static void req_logout(GtkButton *btn, t_chat *chat) {
 
 static void set_profile_info(GtkButton *btn, t_chat *chat) {
     GObject *label_login = gtk_builder_get_object(chat->builder,
-                                                  "label_user_login");
+                                                  "label_profile_login");
     GObject *buff_desc = gtk_builder_get_object(chat->builder,
-                                                "buffer_user_desc");
+                                                "buffer_profile_desc");
 
     gtk_label_set_text(GTK_LABEL(label_login), chat->login);
     gtk_text_buffer_set_text(GTK_TEXT_BUFFER(buff_desc), chat->desc, -1);
+
     (void)btn;
 }
 
