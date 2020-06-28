d4f8210a1ff37df508741a3c90c6a8d1b7094932
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-28 15:50:34 +0300

    refactored get_members

--- server/src/client_handler/api/get_members.c
+++ server/src/client_handler/api/get_members.c
@@ -1,5 +1,35 @@
 #include "api.h"
 
+static gboolean is_valid(t_client *client, guint64 room_id) {
+    if (mx_get_type_member(client->info->database, client->user->user_id,
+                           room_id) == DB_BANNED) {
+        return FALSE;
+    }
+    if (!mx_is_member(client->info->database, client->user->user_id,
+                      room_id)) {
+        return FALSE;
+    }
+    return TRUE;
+}
+
+static gboolean resend(t_client *client, guint64 room_id) {
+    cJSON *members = cJSON_CreateObject();
+    cJSON *json_members = mx_get_json_members(client->info->database,
+                                              room_id);
+    t_dtp *answer = NULL;
+
+    if (!cJSON_AddItemReferenceToObject(members, "members", json_members)
+        ||!cJSON_AddNumberToObject(members, "room_id", room_id)
+        || !cJSON_AddNumberToObject(members, "type", RQ_GET_MEMBERS)) {
+        cJSON_Delete(members);
+        return FALSE;
+    }
+    answer = mx_get_transport_data(members);
+    mx_send(client->out, answer);
+    mx_free_request(&answer);
+    return TRUE;
+}
+
 /*
  * Function: mx_get_members_handler
  * -------------------------------
@@ -12,29 +42,12 @@
  */
 gboolean mx_get_members_handler(t_dtp *data, t_client *client) {
     cJSON *room_id = cJSON_GetObjectItemCaseSensitive(data->json, "room_id");
-    cJSON *members = cJSON_CreateObject();
-    t_dtp *answer = NULL;
 
     if (!cJSON_IsNumber(room_id))
         return FALSE;
-    if (mx_get_type_member(client->info->database, client->user->user_id, room_id->valueint) == DB_BANNED)
+    if (!is_valid(client, room_id->valuedouble))
         return FALSE;
-    if (!mx_is_member(client->info->database, client->user->user_id, room_id->valueint))
+    if (!resend(client, room_id->valuedouble))
         return FALSE;
-    if (!cJSON_AddItemReferenceToObject(members, "members", mx_get_json_members(client->info->database, room_id->valueint))) {
-        cJSON_Delete(members);
-        return FALSE;
-    }
-    if (!cJSON_AddNumberToObject(members, "room_id", room_id->valueint)) {
-        cJSON_Delete(members);
-        return FALSE;
-    }
-    if (!cJSON_AddNumberToObject(members, "type", RQ_GET_MEMBERS)) {
-        cJSON_Delete(members);
-        return FALSE;
-    }
-    answer = mx_get_transport_data(members);
-    mx_send(client->out, answer);
-    mx_free_request(&answer);
     return TRUE;
 }
