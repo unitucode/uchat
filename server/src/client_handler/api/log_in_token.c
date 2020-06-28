7b442b967925555576c8f39cd3f54d58b70d1abf
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-28 12:35:15 +0300

    refactored login by token

--- server/src/client_handler/api/log_in_token.c
+++ server/src/client_handler/api/log_in_token.c
@@ -34,24 +34,24 @@ t_dtp *mx_token_request(char *token, char *login, char *desc) {
  * 
  * returns: success of handling
  */
-bool mx_log_in_token_handler(t_dtp *token, t_client *client) { // TODO!!!!!!
-    cJSON *json = cJSON_Parse(token->str);
-    cJSON *user_token = cJSON_GetObjectItemCaseSensitive(json, "token");
+bool mx_log_in_token_handler(t_dtp *token, t_client *client) {
+    cJSON *user_token = cJSON_GetObjectItemCaseSensitive(token->json,
+                                                         "token");
     t_dtp *reconnect = NULL;
 
-    if (user_token && cJSON_IsString(user_token)
+    if (cJSON_IsString(user_token)
         && mx_isvalid_token(user_token->valuestring)) {
         client->user = mx_get_user_by_token(client->info->database,
                                             user_token->valuestring);
     }
     if (!client->user) {
         mx_logger(MX_LOG_FILE, LOGWAR, "Inccorect token\n");
-        cJSON_Delete(json);
         return false;
     }
-    cJSON_Delete(json);
-    mx_logger(MX_LOG_FILE, LOGMSG, "Logged by token %s\n", client->user->login);
-    reconnect = mx_reconnect_request(client->user->token, client->user->login);
+    mx_logger(MX_LOG_FILE, LOGMSG, "Logged by token %s\n",
+              client->user->login);
+    reconnect = mx_reconnect_request(client->user->token,
+                                     client->user->login);
     mx_send(client->out, reconnect);
     mx_free_request(&reconnect);
     return true;
