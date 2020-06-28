604f7e10bf05da2bc410e94ec0f90fb2f74fe922
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-28 20:21:02 +0300

    refactoread new_member

--- client/src/api/new_member.c
+++ client/src/api/new_member.c
@@ -1,27 +1,38 @@
 #include "client.h"
 
-static gboolean add_member(char *login, int user_id, int room_id, t_chat *chat) {
+static gboolean add_member(char *login, guint64 user_id, guint64 room_id, t_chat *chat) {
     t_groom *groom = mx_get_groom_by_id(room_id, chat->builder);
 
+    if (!groom)
+        return FALSE;
     if (g_hash_table_lookup(groom->members, GINT_TO_POINTER(user_id)))
         return FALSE;
     g_hash_table_insert(groom->members, GINT_TO_POINTER(user_id), g_strdup(login));
-    g_print("new user = %s\n", g_hash_table_lookup(groom->members, GINT_TO_POINTER(user_id)));
     return TRUE;
 }
 
+/*
+ * Function: mx_new_member_handler
+ * -------------------------------
+ * Handles request from server
+ * 
+ * data: request from server
+ * chat: information about chat
+ * 
+ * returns: success of handling
+ */
 gboolean mx_new_member_handler(t_dtp *data, t_chat *chat) {
     cJSON *login = cJSON_GetObjectItemCaseSensitive(data->json, "login");
     cJSON *id = cJSON_GetObjectItemCaseSensitive(data->json, "user_id");
     cJSON *room_id = cJSON_GetObjectItemCaseSensitive(data->json, "room_id");
 
-    if (!cJSON_IsString(login))
+    if (!cJSON_IsString(login) || !cJSON_IsNumber(id)
+        || !cJSON_IsNumber(room_id)) {
         return FALSE;
-    if (!cJSON_IsNumber(id))
+    }
+    if (!add_member(login->valuestring, id->valuedouble,
+                    room_id->valuedouble, chat)) {
         return FALSE;
-    if (!cJSON_IsNumber(room_id))
-        return FALSE;
-    if (!add_member(login->valuestring, id->valueint, room_id->valueint, chat))
-        return FALSE;
+    }
     return TRUE;
 }
