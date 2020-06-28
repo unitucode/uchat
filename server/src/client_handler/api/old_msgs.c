6754788851d09eb25b23552cdc1060ee38dd7df9
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-28 14:02:49 +0300

    refactored old_msgs

--- server/src/client_handler/api/old_msgs.c
+++ server/src/client_handler/api/old_msgs.c
@@ -1,5 +1,30 @@
 #include "api.h"
 
+static gboolean is_valid(t_client *client, guint64 room_id) {
+    if (!mx_is_member(client->info->database, client->user->user_id,
+                      room_id)) {
+        return FALSE;
+    }
+    if (mx_get_type_member(client->info->database, client->user->user_id,
+                           room_id) == DB_BANNED) {
+        return FALSE;
+    }
+    return TRUE;
+}
+
+static gboolean resend(t_client *client, cJSON *msgs_json) {
+    t_dtp *msgs = NULL;
+
+    if (!cJSON_AddNumberToObject(msgs_json, "type", RQ_OLD_MSGS)) {
+        cJSON_Delete(msgs_json);
+        return FALSE;
+    }
+    msgs = mx_get_transport_data(msgs_json);
+    mx_send(client->out, msgs);
+    mx_free_request(&msgs);
+    return TRUE;
+}
+
 /*
  * Function: mx_old_msgs_handler
  * -------------------------------
@@ -14,26 +39,16 @@ gboolean mx_old_msgs_handler(t_dtp *data, t_client *client) {
     cJSON *date = cJSON_GetObjectItemCaseSensitive(data->json, "date");
     cJSON *count = cJSON_GetObjectItemCaseSensitive(data->json, "count");
     cJSON *room_id = cJSON_GetObjectItemCaseSensitive(data->json, "room_id");
-    t_dtp *msgs = NULL;
     cJSON *msgs_json = NULL;
 
-    if (!cJSON_IsNumber(date))
-        return FALSE;
-    if (!cJSON_IsNumber(count))
-        return FALSE;
-    if (!cJSON_IsNumber(room_id))
-        return FALSE;
-    if (!mx_is_member(client->info->database, client->user->user_id, room_id->valueint))
-        return FALSE;
-    if (mx_get_type_member(client->info->database, client->user->user_id, room_id->valueint) == DB_BANNED)
-        return FALSE;
-    msgs_json = mx_get_old_messages_by_id(client->info->database, room_id->valueint, date->valuedouble, count->valueint);
-    if (!cJSON_AddNumberToObject(msgs_json, "type", RQ_OLD_MSGS)) {
-        cJSON_Delete(msgs_json);
+    if (!cJSON_IsNumber(room_id)  || !cJSON_IsNumber(count)
+        || !cJSON_IsNumber(date) || !is_valid(client, room_id->valuedouble)) {
         return FALSE;
     }
-    msgs = mx_get_transport_data(msgs_json);
-    mx_send(client->out, msgs);
-    mx_free_request(&msgs);
+    msgs_json = mx_get_old_messages_by_id(
+        client->info->database, room_id->valueint,
+        date->valuedouble, count->valueint);
+    if (!resend(client, msgs_json))
+        return FALSE;    
     return TRUE;
 }
