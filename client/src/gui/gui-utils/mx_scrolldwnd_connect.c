e9166009dd58d7324d2eb45fa1714fcc9d3ee90f
vstruk01 <stoliar444@gmail.com>
2020-06-28 15:42:22 +0300

    fix gtk_adjustment for delete message

--- client/src/gui/gui-utils/mx_scrolldwnd_connect.c
+++ client/src/gui/gui-utils/mx_scrolldwnd_connect.c
@@ -3,30 +3,15 @@
 static void mx_scroll_to_end(GtkAdjustment *adj, gpointer user_data) {
     static gdouble last_value = 0;
     static gdouble last_upper = 0;
-    t_groom *room = (t_groom*)user_data;
     gdouble upper = gtk_adjustment_get_upper(adj);
-    gdouble upper_f = upper - last_upper;
+    gdouble value = gtk_adjustment_get_value(adj);
 
-    gdouble value = last_value + upper_f;
-    gtk_adjustment_set_value(adj, value);
-    if (room->uploaded == MX_BUF_MSGS) {
-        if (last_upper) {
-            upper = last_upper;
-            gtk_adjustment_set_upper(adj, upper);
-        }
+    if (value > (last_upper - gtk_adjustment_get_page_size(adj) - 50) || value == 0) {
+        gtk_adjustment_set_value(adj, upper - gtk_adjustment_get_page_size(adj));
     }
-    else if (room->uploaded > MX_BUF_MSGS) {}
-    value = last_value + upper_f;
-    gtk_adjustment_set_value(adj, value);
-
-    g_print("\nvalue    == %f\n", gtk_adjustment_get_value(adj));
-    g_print("last_value == %f\n\n", last_value);
-    g_print("\nlower      == %f\n", gtk_adjustment_get_lower(adj));
-    g_print("upper      == %f\n", gtk_adjustment_get_upper(adj));
-    g_print("last_upper == %f\n", last_upper);
-    g_print("page_size  == %f\n\n", gtk_adjustment_get_page_size(adj));
     last_value = gtk_adjustment_get_value(adj);
     last_upper = gtk_adjustment_get_upper(adj);
+    (void)user_data;
 }
 
 /*
--- client/src/gui/mx_messages_top_reached.c
+++ client/src/gui/mx_messages_top_reached.c
@@ -26,10 +26,18 @@ void mx_box_messages_reached(GtkScrolledWindow *scroll,
         gtk_adjustment_set_value(adj, gtk_adjustment_get_value(adj) + 1);
     }
     if (pos == GTK_POS_BOTTOM) {
+
         chat->upl_old_msgs = true;
         while (groom->uploaded > MX_BUF_MSGS) {
+            GtkRequisition req;
+            GtkRequisition req2;
+            gtk_widget_get_preferred_size(GTK_WIDGET(gtk_list_box_get_row_at_index(groom->box_messages, 0)), &req, &req2);
+            GtkAdjustment *adj = gtk_scrolled_window_get_vadjustment(scroll);
+
             puts("DELETE OLD MSG");
             delete_older_message(groom);
+            gtk_adjustment_set_upper(adj, gtk_adjustment_get_upper(adj) - req2.height);
+            gtk_adjustment_set_value(adj, gtk_adjustment_get_upper(adj) - gtk_adjustment_get_page_size(adj));
             groom->uploaded--;
         }
         chat->upl_old_msgs = false;
