c1e9d053ea4a19810990a8f46879b71e854672cc
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-28 13:52:12 +0300

    refactored search_msgs

--- server/src/client_handler/api/search_msgs.c
+++ server/src/client_handler/api/search_msgs.c
@@ -34,15 +34,19 @@ gboolean mx_search_msgs_handler(t_dtp *data, t_client *client) {
     cJSON *room_id = cJSON_GetObjectItemCaseSensitive(data->json, "room_id");
     t_dtp *answer = NULL;
 
-    if (!cJSON_IsString(msg))
+    if (!cJSON_IsString(msg) || !cJSON_IsNumber(room_id))
         return FALSE;
-    if (!cJSON_IsNumber(room_id))
+    if (!mx_is_member(client->info->database, client->user->user_id,
+                      room_id->valueint)) {
         return FALSE;
-    if (!mx_is_member(client->info->database, client->user->user_id, room_id->valueint))
+    }
+    if (mx_get_type_member(client->info->database, client->user->user_id,
+                           room_id->valueint) == DB_BANNED) {
         return FALSE;
-    if (mx_get_type_member(client->info->database, client->user->user_id, room_id->valueint) == DB_BANNED)
-        return FALSE;
-    answer = mx_search_msgs_request(mx_search_message(client->info->database, msg->valuestring, room_id->valueint));
+    }
+    answer = mx_search_msgs_request(
+        mx_search_message(client->info->database, msg->valuestring,
+                          room_id->valueint));
     mx_send(client->out, answer);
     mx_free_request(&answer);
     return TRUE;
