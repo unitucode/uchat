a132d338e545fe961f422ff64c9991d7d1d47c1f
homin.sashawork@gmail.com <homin.sashawork@gmail.com>
2020-06-28 14:16:57 +0300

    refactored del_room

--- server/src/client_handler/api/del_room.c
+++ server/src/client_handler/api/del_room.c
@@ -31,13 +31,15 @@ t_dtp *mx_delete_room_request(int room_id) {
  */
 gboolean mx_del_room_handler(t_dtp *data, t_client *client) {
     cJSON *room_id = cJSON_GetObjectItemCaseSensitive(data->json,
-                                                   "room_id");
+                                                      "room_id");
     t_dtp *resend = NULL;
 
     if (!cJSON_IsNumber(room_id))
         return FALSE;
-    if (mx_get_type_member(client->info->database, client->user->user_id, room_id->valueint) != DB_CUSTOMER)
+    if (mx_get_type_member(client->info->database, client->user->user_id,
+                           room_id->valueint) != DB_CUSTOMER) {
         return FALSE;
+    }
     resend = mx_delete_room_request(room_id->valueint);
     mx_send_to_all(resend, client, room_id->valueint);
     mx_delete_room_by_id(client->info->database, room_id->valueint);
