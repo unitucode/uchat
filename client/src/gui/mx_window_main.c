b3093ff49c7eeaa3ae34e1231d6f7cd6b1d5d017
Dumagenon <simonov.pasha9822@gmail.com>
2020-06-30 19:18:54 +0300

    merge with gui-recv + switch theme added

--- client/src/gui/mx_window_main.c
+++ client/src/gui/mx_window_main.c
@@ -7,7 +7,6 @@ GtkBuilder *mx_init_window(int argc, char **argv) {
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, MX_GUI_PATH, NULL);
     gtk_builder_connect_signals(builder, builder);
-    mx_css_connect();
     return builder;
 }
 
