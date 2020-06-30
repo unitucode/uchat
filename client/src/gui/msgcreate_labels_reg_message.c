a39801917d413310ca9821fc478afddfb5b1bb32
Yurii Makohon <yuriimakohon@gmail.com>
2020-06-30 20:04:00 +0300

    go-down button work

--- client/src/gui/msgcreate_labels_reg_message.c
+++ client/src/gui/msgcreate_labels_reg_message.c
@@ -58,17 +58,14 @@ void mx_msgcreate_label_time(GtkWidget *box_info,
 void download(GtkButton *btn, t_chat *chat) {
     t_groom *groom = mx_get_selected_groom(chat->builder, MX_LOCAL_ROOMS);
     t_gmsg *gmsg = g_object_get_data(G_OBJECT(btn), "gmsg");
+    t_signal_data *data = g_object_get_data(G_OBJECT(gmsg->row_msg),
+                                            "sigdata");
 
-    t_signal_data *data = g_object_get_data(G_OBJECT(gmsg->row_msg), "sigdata");
-    if (!data)
-        puts("NULL");
     mx_select_msg(NULL, NULL, data);
-    if (g_file_test(gmsg->msg, G_FILE_TEST_EXISTS)) {
-        g_message("%s EXIST", gmsg->msg);
-    }
-    else {
+    if (g_file_test(gmsg->msg, G_FILE_TEST_EXISTS))
+        system("open files/");
+    else
         mx_download_file(groom->id, gmsg->message_id, chat);
-    }
 }
 
 void mx_msgcreate_file(GtkWidget *box_info, t_gmsg *gmsg,
