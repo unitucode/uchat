96d5d791ef33d1c1583e94c7cd0c2d20e004e55c
Yurii Makohon <yuriimakohon@gmail.com>
2020-07-02 17:36:03 +0300

    gif added

--- client/src/gui/mx_open_files_dir.c
+++ client/src/gui/mx_open_files_dir.c
@@ -1,6 +1,6 @@
 #include "client.h"
 
-static void open_dir(gchar *filename) {
+void mx_open_file_folder(gchar *filename) {
     gchar *path = g_path_get_dirname(filename);
     GFile *file = g_file_new_for_path(path);
     gchar *uri = NULL;
@@ -28,6 +28,11 @@ static gboolean show_image_viewer(gchar *file_path, GtkBuilder *builder) {
         gtk_image_set_from_pixbuf(GTK_IMAGE(img), GDK_PIXBUF(pixbuf));
         return TRUE;
     }
+    else if (g_str_has_suffix(file_path, ".gif")) {
+        mx_widget_set_visibility(GTK_WIDGET(viewer), TRUE);
+        gtk_image_set_from_animation(GTK_IMAGE(img), gdk_pixbuf_animation_new_from_file(file_path, NULL));
+        return TRUE;
+    }
     return FALSE;
 }
 
@@ -41,7 +46,7 @@ void mx_open_files_dir(GtkButton *btn, t_chat *chat) {
     mx_select_msg(NULL, NULL, data);
     if (g_file_test(gmsg->msg, G_FILE_TEST_EXISTS)) {
         if (!show_image_viewer(gmsg->msg, chat->builder))
-            open_dir(gmsg->msg);
+            mx_open_file_folder(gmsg->msg);
     }
     else {
         mx_download_file(groom->id, gmsg->message_id, chat);
