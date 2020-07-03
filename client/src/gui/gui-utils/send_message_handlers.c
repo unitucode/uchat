623aa91c4b209fd6051fa6775d34e530a2dc57c3
Dumagenon <simonov.pasha9822@gmail.com>
2020-07-03 16:43:53 +0300

    replace sleep to g_usleep

--- client/src/gui/gui-utils/send_message_handlers.c
+++ client/src/gui/gui-utils/send_message_handlers.c
@@ -6,7 +6,7 @@ void mx_send_message_handle_enter(GtkWidget *textview,
                                   GdkEvent *event, t_chat *chat) {
     if (event->key.keyval == (gint)65505)
         shift_hold = TRUE;
-    sleep(0);
+    g_usleep(0);
     if (event->key.keyval == (gint)65293 && !shift_hold) {
         mx_req_send_message(NULL, chat);
         mx_clear_buffer_text("buffer_message", chat->builder);
