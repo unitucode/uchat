5be1f50a1fa4085d348c6205e7a8ad1a73782420
Yurii Makohon <yuriimakohon@gmail.com>
2020-07-04 12:57:36 +0300

    added some coments to client.h

--- client/inc/client.h
+++ client/inc/client.h
@@ -7,12 +7,18 @@
 #include <glib.h>
 #include <gio/gio.h>
 
+/*
+ * Icons and images paths
+ */
 #define MX_IMGS_PATH "../src/gui/resources/"
 #define MX_STICKER_PATH MX_IMGS_PATH"stickers/"
 #define MX_GUI_PATH "../src/gui/gui.glade"
 #define MX_IMG_EYE MX_IMGS_PATH"eye.png"
 #define MX_IMG_CLOSEDEYE MX_IMGS_PATH"closed-eye.png"
 
+/*
+ * Error messages for authorization
+ */
 #define MX_ERRMSG_INVALID_LOGIN "Login can be minimum 3 symbol of a-z, 0-9, -"
 #define MX_ERRMSG_NODATA "Please, enter login and password"
 #define MX_ERRMSG_DIFPASS "Passwords must match"
@@ -20,12 +26,21 @@
 #define MX_ERRMSG_USEREXIST "User already exist"
 #define MX_ERRMSG_CLIEXIST "User already authorized"
 
+/*
+ * Controll-headerbar
+ */
 #define MX_ROOM_CTRL 0
 #define MX_MSG_CTRL 1
 
+/*
+ * GtkListBox of room lists
+ */
 #define MX_LOCAL_ROOMS "listbox_rooms"
 #define MX_GLOBAL_ROOMS "listbox_global_rooms"
 
+/*
+ * Themes
+ */
 #define MX_DARK_THEME "../src/gui/resources/dark-theme.css"
 #define MX_LIGHT_THEME "../src/gui/resources/light-theme.css"
 #define MX_THEME_FILE "../set-theme"
@@ -117,22 +132,22 @@ typedef struct s_gmsg t_gmsg;
  * error_handler: array of error handlers
  * request_hanlder: array of request handlers
  * msg_placeholder: status of placeholder
- * css_prov:
+ * css_prov: contain css-styles
  */
 typedef struct s_chat t_chat;
 
 /* Filter data
  * ----------
- * is_found_rooms:
- * search_name:
+ * is_found_rooms: flag of found rooms
+ * search_name: contain search name of room
  */
 typedef struct s_filter_data t_filter_data;
 
 /* Signal data
  * ----------
- * groom:
- * chat:
- * row_msg:
+ * groom: t_groom for gpointer user_data
+ * chat: t_chat for gpointer user_data
+ * row_msg: GtkListBoxRow for gpointer user_data
  */
 typedef struct s_signal_data t_signal_data;
 
@@ -379,7 +394,9 @@ void mx_reset_select_count(void);
 void mx_open_files_dir(GtkButton *btn, t_chat *chat);
 void mx_req_send_message(GtkButton *btn, t_chat *chat);
 
-// gui utils
+/*
+ * Gui utils
+ */
 void mx_scrlldwnd_connect(gchar *name, GtkWidget *scroll, GtkBuilder *builder,
                           t_groom *room);
 gchar *mx_entry_get_text(gchar *entry_name, GtkBuilder *builder);
