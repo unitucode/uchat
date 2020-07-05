ac6849c32d995aed17dee2dbbe79f1f871bb0389
Yurii Makohon <yuriimakohon@gmail.com>
2020-07-05 17:38:54 +0300

    dynamic delete messages unselect all messages now

--- client/src/gui/gui_update_msg.c
+++ client/src/gui/gui_update_msg.c
@@ -35,9 +35,9 @@ void mx_gdel_msg(guint64 msg_id, guint64 room_id, GtkBuilder *builder) {
     t_groom *room = mx_get_groom_by_id(room_id, builder);
 
     if (msg && room) {
+        mx_unselect_curr_room_messages(builder);
         room->uploaded--;
         mx_delete_row_msg(msg->row_msg, builder);
-        mx_unselect_curr_room_messages(builder);
     }
 }
 
