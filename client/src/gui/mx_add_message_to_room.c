8a23d4a03df004f0aa09a8c6ba743a57b6366e39
Yurii Makohon <yuriimakohon@gmail.com>
2020-07-05 14:06:59 +0300

    Auditor fixed

--- client/src/gui/mx_add_message_to_room.c
+++ client/src/gui/mx_add_message_to_room.c
@@ -1,10 +1,21 @@
 #include "client.h"
 
-// static void add_sigdata(GtkWidget *row, GtkWidget *event, t_chat *chat) {
-// }
+static void add_sigdata(GtkWidget *row, GtkWidget *event, t_chat *chat) {
+    t_signal_data *data = NULL;
 
-// static void add_row(GtkWidget *row, GtkWidget *event, t_gmsg *msg) {
-// }
+    gtk_widget_set_can_focus(row, FALSE);
+    data = mx_create_sigdata(chat, NULL, GTK_LIST_BOX_ROW(row));
+    g_signal_connect(event, "button_release_event",
+                    G_CALLBACK(mx_select_msg), data);
+    g_object_set_data_full(G_OBJECT(row), "sigdata", data,
+                        (GDestroyNotify)mx_free_sigdata);
+}
+
+static void add_row(GtkWidget *row, GtkWidget *event, t_gmsg *msg) {
+        msg->row_msg = GTK_LIST_BOX_ROW(row);
+        g_object_ref(row);
+        gtk_container_add(GTK_CONTAINER(row), event);
+}
 
 static void add_message_row(t_gmsg *msg, t_chat *chat, gint position) {
     t_groom *groom = mx_get_groom_by_id(msg->room_id, chat->builder);
@@ -15,20 +26,9 @@ static void add_message_row(t_gmsg *msg, t_chat *chat, gint position) {
     if (!groom)
         return;
     box = groom->box_messages;
-    // add_sigdata(row, event, chat);
-    // add_row(&row, event, msg);
-        t_signal_data *data = NULL;
+    add_sigdata(row, event, chat);
+    add_row(row, event, msg);
 
-        gtk_widget_set_can_focus(row, FALSE);
-        data = mx_create_sigdata(chat, NULL, GTK_LIST_BOX_ROW(row));
-        g_signal_connect(event, "button_release_event",
-                        G_CALLBACK(mx_select_msg), data);
-        g_object_set_data_full(G_OBJECT(row), "sigdata", data,
-                            (GDestroyNotify)mx_free_sigdata);
-
-        msg->row_msg = GTK_LIST_BOX_ROW(row);
-        g_object_ref(row);
-        gtk_container_add(GTK_CONTAINER(row), event);
     groom->is_updated = TRUE;
     gtk_list_box_insert(box, row, position);
     gtk_list_box_row_changed(groom->row_room);
