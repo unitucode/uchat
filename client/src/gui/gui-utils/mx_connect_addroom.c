e346a277dae7a7071635da435a246f0d7bf05d86
Yurii Makohon <yuriimakohon@gmail.com>
2020-07-05 19:21:47 +0300

    removed gmassage

--- client/src/gui/gui-utils/mx_connect_addroom.c
+++ client/src/gui/gui-utils/mx_connect_addroom.c
@@ -3,7 +3,7 @@
 static void req_addroom(GtkButton *btn, t_chat *chat) {
     gchar *room_name = mx_get_buffer_text("buffer_roomname", chat->builder);
     t_dtp *dtp = mx_new_room_request(room_name, "", DB_GLOBAL_CHAT);
-g_message("%s: %lu\n", room_name, strlen(room_name));
+
     mx_send(chat->out, dtp);
     mx_free_request(&dtp);
     mx_reset_addroom(NULL, chat->builder);
